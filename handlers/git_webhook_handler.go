c76c7f37bc36c4f16561c2765db0774271731b5a
Dinneya Charles <dinneyacharles007@gmail.com>
2024-12-11 20:06:48 +0100

    change to sse

--- handlers/git_webhook_handler.go
+++ handlers/git_webhook_handler.go
@@ -13,14 +13,12 @@ type GitWebhookHandler struct {
 }
 
 func (h *GitWebhookHandler) ReceivePushEvent(w http.ResponseWriter, r *http.Request) {
-
-	h.Stream.Eventlog.Clear()
-
 	defer func(Body io.ReadCloser) {
 		err := Body.Close()
 		if err != nil {
 			fmt.Println(err)
 		}
+		h.Stream.Eventlog.Clear()
 	}(r.Body)
 
 	b, err := io.ReadAll(r.Body)
@@ -36,17 +34,6 @@ func (h *GitWebhookHandler) ReceivePushEvent(w http.ResponseWriter, r *http.Requ
 }
 
 func (h *GitWebhookHandler) Listen(w http.ResponseWriter, r *http.Request) {
-
-	w.Header().Set("Content-Type", "text/event-stream")
-	w.Header().Set("Cache-Control", "no-cache")
-	w.Header().Set("Connection", "keep-alive")
-
-	//for x := range h.Events {
-	//	_, _ = fmt.Fprintf(w, "data: %v\n\n", x)
-	//	w.(http.Flusher).Flush() // Send the event immediately
-	//}
-	//
-	//h
 
 	h.SseServer.ServeHTTP(w, r)
 }
